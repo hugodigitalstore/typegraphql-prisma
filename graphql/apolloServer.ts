a3c03955655538d28ea3ba7565f78bd9ab8554bb
Hugo <huseyin@gozutok.info>
2022-04-24 12:23:44 +0300

    .

--- graphql/apolloServer.ts
+++ graphql/apolloServer.ts
@@ -1,10 +1,13 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { schema } from './schema';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 
 const server = new ApolloServer({
     schema,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req, prisma })
 })
 
 export { server };
