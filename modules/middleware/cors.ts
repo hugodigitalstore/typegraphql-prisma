ef2cfd82cd288b602245e954f0ae82c27ef43b1a
Hugo <huseyin@gozutok.info>
2022-04-25 12:57:51 +0300

    cors

--- modules/middleware/cors.ts
+++ modules/middleware/cors.ts
@@ -1,10 +1,11 @@
 import Cors from "cors";
 import initMiddleware from "./initMiddleware";
 
+let corsUrl: string = process.env.NEXT_PUBLIC_APOLLO_URL || "https://studio.apollographql.com";
 const cors = initMiddleware(
     Cors({
         credentials: true,
-        origin: ["https://studio.apollographql.com"],
+        origin: [corsUrl],
     })
 );
 
